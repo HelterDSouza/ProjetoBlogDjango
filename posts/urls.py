468a5d9927066645807702b25f38f5e16e83ad7a
helter daniel <helterdaniel@hotmail.com>
2021-07-23 16:35:51 -0300

    adicionado filtro por categoria e publicado

--- posts/urls.py
+++ posts/urls.py
@@ -8,4 +8,10 @@ app_name = "posts"
 urlpatterns = [
     path("", views.PostIndex.as_view(), name="index"),
     path("post/<int:pk>", views.PostDetalheUpdateView.as_view(), name="post_detalhes"),
+    path(
+        "categoria/<str:categoria>",
+        views.PostCategoriaListView.as_view(),
+        name="post_categoria",
+    ),
+    path("busca/", views.PostBuscaListView.as_view(), name="post_busca"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
 from typing import ItemsView
 
+from categorias.models import Categoria
+from django.db.models import Case, Count, Q, When
 from django.shortcuts import render
 from django.views.generic.edit import UpdateView
 from django.views.generic.list import ListView
@@ -12,7 +14,37 @@ class PostIndex(ListView):
     model = Post
     template_name = "posts/index.html"
     context_object_name = "posts"
+    paginate_by = 6
+
+    def get_queryset(self):
+        qs = super().get_queryset().filter(publicado=True)
+        qs = qs.order_by("-id")
+        qs = qs.annotate(
+            numero_comentarios=Count(
+                Case(When(post_comentario__publicado_comentario=True, then=1))
+            )
+        )
+        return qs
 
 
 class PostDetalheUpdateView(UpdateView):
     model = Post
+
+
+class PostCategoriaListView(PostIndex):
+    template_name = "posts/post_categoria.html"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        categoria = self.kwargs.get("categoria", None)
+        if not categoria:
+            return qs
+        qs = qs.filter(categoria__nome_categoria__iexact=categoria)
+        return qs
+
+
+class PostBuscaListView(PostIndex):
+    template_name = "posts/post_busca.html"
+
+
+
